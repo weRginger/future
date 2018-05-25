91eed680223626136bc31d2f842d0cd8d374c58f
ziqif <ziqifan16@gmail.com>
2018-05-25 16:04:15 -0700

    more

---
+++ li/46.permutations.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        if(nums.empty()) return ans;
+        vector<bool> visited(nums.size(), false);
+        vector<int> curr;
+        helper(nums, ans, curr, visited);
+        return ans;
+    }
+private:
+    void helper(const vector<int>& nums, vector<vector<int>>& ans, vector<int>& curr, vector<bool>& visited) {
+        if(curr.size() == nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            if(visited[i] == false) {
+                curr.push_back(nums[i]);
+                visited[i] = true;
+                helper(nums, ans, curr, visited);
+                visited[i] = false;
+                curr.pop_back();
+            }
+        }
+    }
+};
