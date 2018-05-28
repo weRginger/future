59a33930199a8a20338ca11c3141f38e4f5c3418
Ziqi Fan <ziqifan16@gmail.com>
2018-05-28 14:51:07 -0700

    more

---
+++ li/605.can-place-flowers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if(n == 0) return true;
+        if(flowerbed.empty()) return false;
+        int s = flowerbed.size();
+        for(int i = 0; i < s; i++) {
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == s - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+            }
+            if(n == 0) return true;
+        }
+        return false;
+    }
+};
