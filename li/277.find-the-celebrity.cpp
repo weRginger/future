56dbe4a9121f6f5d3b411cba98003910fc62590f
BEaFAN <ziqifan16@gmail.com>
2020-05-04 16:21:24 -0700

    Update 277.find-the-celebrity.cpp

---
+++ li/277.find-the-celebrity.cpp
@@ -0,0 +1,20 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        if (n == 0) return -1;
+        int c = 0;
+        for (int i = 1; i < n; i++) {
+            if(!knows(i, c)) {
+                c = i;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i != c && (knows(c, i) || !knows(i, c)))
+                return -1;
+        }
+        return c;
+    }
+};
