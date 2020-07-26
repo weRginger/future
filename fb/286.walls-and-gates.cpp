7ad387901fa8a79190ee989ddceffa1a2707537c
BEaFAN <ziqifan16@gmail.com>
2020-07-26 11:45:17 -0700

    Delete 116.populating-next-right-pointers-in-each-node.cpp

--- fb/286.walls-and-gates.cpp
+++ fb/286.walls-and-gates.cpp
@@ -1,40 +1,42 @@
-// BFS start from gate 
-// Time: O(mn)
-// If BFS start from empty room, time O(m^2 * n^2)
+// Great question
+// Time O(m*n)
+// Space O(m*n)
 class Solution {
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
-        const auto INF = 2147483647;
-				
-        queue<pair<int, int>> coords;
-        for (int i = 0; i < rooms.size(); ++i) {
-            for (int j = 0; j < rooms[i].size(); ++j) {
-                if (rooms[i][j] == 0) {
-                    coords.push({i, j});
-                }
+        if(rooms.empty() || rooms[0].empty()) 
+            return;
+        int rs = rooms.size();
+        int cs = rooms[0].size();
+        
+        queue<pair<int, int>> q;
+        for(int i = 0; i < rs; i++) {
+            for(int j = 0; j < cs; j++) {
+                if(rooms[i][j] == 0)
+                    q.push({i, j});
             }
         }
-
-        while (!coords.empty()) {
-            auto coord = coords.front();
-            coords.pop();
-            int row = coord.first;
-            int col = coord.second;
-            if (row + 1 < rooms.size() && rooms[row + 1][col] == INF) {
-                rooms[row + 1][col] = rooms[row][col] + 1;
-                coords.push({row + 1, col});
-            }
-            if (row - 1 >= 0 && rooms[row - 1][col] == INF) {
-                rooms[row - 1][col] = rooms[row][col] + 1;
-                coords.push({row - 1, col});
-            }
-            if (col + 1 < rooms[row].size() && rooms[row][col + 1] == INF) {
-                rooms[row][col + 1] = rooms[row][col] + 1;
-                coords.push({row, col + 1});
-            }
-            if (col - 1 >= 0 && rooms[row][col - 1] == INF) {
-                rooms[row][col - 1] = rooms[row][col] + 1;
-                coords.push({row, col - 1});
+        
+        vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while(!q.empty()) {
+            auto p = q.front();
+            q.pop();
+            int r = p.first;
+            int c = p.second;
+            for(int j = 0; j < 4; j++) {
+                int row = r + dir[j][0];
+                int col = c + dir[j][1];
+                
+                if(row < 0 || 
+                   row >= rs || 
+                   col < 0 || 
+                   col >= cs || 
+                   rooms[row][col] != INT_MAX) {
+                    continue;
+                }
+                    
+                rooms[row][col] = rooms[r][c] + 1;
+                q.push({new_r, col});
             }
         }
     }
