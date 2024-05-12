3eb8ce0172a4d7862c6ded2649a3f11159f0719b
futureIsNow <ziqifan16@gmail.com>
2024-05-12 09:46:53 -0700

    Update 938.Range-Sum-of-BST.cpp

--- fb/938.Range-Sum-of-BST.cpp
+++ fb/938.Range-Sum-of-BST.cpp
@@ -22,6 +22,26 @@ public:
     }
 };
 
+// Time O(n)
+// Space O(H)
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root) return 0;
+        int ans = 0;
+        if(root->val >= low && root->val <= high) {
+            ans += root->val;
+            ans += rangeSumBST(root->left, low, high);
+            ans += rangeSumBST(root->right, low, high);
+        } else if(root->val > high) {
+            ans += rangeSumBST(root->left, low, high);
+        } else {
+            ans += rangeSumBST(root->right, low, high);
+        }
+        return ans;
+    }
+};
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
