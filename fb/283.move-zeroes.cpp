9367a2f444297fa3f7cc729b9aac467baf35c6b9
futureIsNow <ziqifan16@gmail.com>
2024-05-14 22:46:36 -0700

    Update 283.move-zeroes.cpp

--- fb/283.move-zeroes.cpp
+++ fb/283.move-zeroes.cpp
@@ -1,5 +1,27 @@
 // Time O(n)
 // Space O(1)
+// ziqi's solution
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int sz = nums.size();
+        for(int i = 0, zero_idx = sz; i < sz; i++) {
+            if(nums[i] == 0) {
+                if(zero_idx == sz) { // using sz to indicate whether we have seen 0
+                    zero_idx = i;
+                }
+            } else {
+                if(zero_idx < i) { // means we have seen 0
+                    nums[zero_idx++] = nums[i];
+                    nums[i] = 0;
+                }
+            }
+        }
+    }
+};
+
+// Time O(n)
+// Space O(1)
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
