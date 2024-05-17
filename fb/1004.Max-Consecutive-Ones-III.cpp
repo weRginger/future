f197f4be5508b334099fb4fb2d84b5cf6fa58713
futureIsNow <ziqifan16@gmail.com>
2024-05-16 19:50:15 -0700

    Update 1004.Max-Consecutive-Ones-III.cpp

--- fb/1004.Max-Consecutive-Ones-III.cpp
+++ fb/1004.Max-Consecutive-Ones-III.cpp
@@ -4,17 +4,17 @@
 // details: https://leetcode.com/problems/max-consecutive-ones-iii/solution/
 class Solution {
 public:
-    int longestOnes(vector<int>& A, int K) {
+    int longestOnes(vector<int>& nums, int k) {
         int left = 0, right;
-        for (right = 0; right < A.size(); right++) {
-            // If we included a zero in the window we reduce the value of K.
-            // Since K is the maximum zeros allowed in a window.
-            if (A[right] == 0) K--;
+        for (right = 0; right < nums.size(); right++) {
+            // If we included a zero in the window we reduce the value of k.
+            // Since k is the maximum zeros allowed in a window.
+            if (nums[right] == 0) k--;
             // A negative K denotes we have consumed all allowed flips and window has
             // more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
-            if (K < 0) {
-                // If the left element to be thrown out is zero we increase K.
-                if (A[left] == 0) K++;
+            if (k < 0) {
+                // If the left element to be thrown out is zero we increase k.
+                if (nums[left] == 0) k++;
                 left++;
             }
         }
