1827ee544c8e7a35f9ab5dbc0bf5e85ef97dd032
futureIsNow <ziqifan16@gmail.com>
2024-05-17 19:39:04 -0700

    Update 38.count-and-say.cpp

--- fb/38.count-and-say.cpp
+++ fb/38.count-and-say.cpp
@@ -1,20 +1,24 @@
+// time O(n*k)
+// space O(k)
 class Solution {
 public:
     string countAndSay(int n) {
-        if(n < 1) return string();
         string ans = "1";
-        for(int i = 2; i <= n; i++) {
+        for(int i = 1; i < n; i++) { // skip n == 1, base case
             string next = "";
-            int j = 1, count = 1;
-            for(; j < ans.size(); j++) {
-                if(ans[j] == ans[j-1])
-                    count++;
-                else {
-                    next += to_string(count) + ans[j-1];
-                    count = 1;
+            int j = 1, freq = 1;
+            while(j < ans.size()) {
+                if(ans[j] == ans[j-1]) {
+                    freq++;
+                } else {
+                    next.push_back('0' + freq);
+                    next.push_back(ans[j-1]);
+                    freq = 1;
                 }
+                j++;
             }
-            next += to_string(count) + ans[j-1];
+            next.push_back('0' + freq);
+            next.push_back(ans[j-1]);
             ans = next;
         }
         return ans;
