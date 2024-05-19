143f161ff409b58d4778ba4534ecdcdbb58e4a71
futureIsNow <ziqifan16@gmail.com>
2024-05-19 00:37:57 -0700

    Create facebook-onsite-generate-random-max-index.cpp

---
+++ fb/facebook-onsite-generate-random-max-index.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <limits.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxRandomIndex(vector<int>& nums) {
+        vector<int> ans;
+        int max = INT_MIN;
+        int maxIndex = -1;
+        int count = 0;
+        for (int i = 0; i < int(nums.size()); i++) {
+          if (nums[i] > max) {
+            max = nums[i];
+            maxIndex = i;
+            count = 1;
+          } else if (nums[i] == max) {
+            count++;
+            // reservoir sampling to choose 1 random index among count up to now
+            if (rand() % count == 0) {
+              maxIndex = i;
+            }
+          }
+          ans.push_back(maxIndex);
+        }
+        return ans;
+    }
+};
+
+// To execute C++, please define "int main()"
+int main() {
+  srand(time(0));
+  Solution sol = Solution();
+  vector<int> nums{11, 30, 2, 30, 30, 30, 6, 2, 62, 62}; 
+  vector<int> ans = sol.maxRandomIndex(nums);
+  for(auto a : ans) {
+    cout << a << " ";
+  }
+  cout << "done";
+  return 0;
+}
