b35fc4ca5d25e5c154f72c2a30d421f3df3ca587
futureIsNow <ziqifan16@gmail.com>
2024-05-22 23:42:23 -0700

    Update 265.paint-house-ii.cpp

--- fb/265.paint-house-ii.cpp
+++ fb/265.paint-house-ii.cpp
@@ -1,18 +1,54 @@
+// Time O(n*k)
+// Space O(1)
 class Solution {
 public:
     int minCostII(vector<vector<int>>& costs) {
-        if (costs.empty()) return 0;
-        int n = costs.size(), k = costs[0].size(), m1 = 0, m2 = 0;
-        vector<int> dp(k, 0);
-        for (int i = 0; i < n; i++) {
-            int t1 = m1, t2 = m2;
-            m1 = m2 = INT_MAX;
-            for (int j = 0; j < k; j++) {
-                dp[j] = (dp[j] != t1 ? t1 : t2) + costs[i][j];
-                if (dp[j] < m1) m2 = m1, m1 = dp[j];
-                else m2 = min(m2, dp[j]);
+        if (costs.size() == 0) return 0;
+        int k = costs[0].size();
+        int n = costs.size();
+
+
+        /* Firstly, we need to determine the 2 lowest costs of
+         * the first row. We also need to remember the color of
+         * the lowest. */
+        int prevMin = -1; int prevSecondMin = -1; int prevMinColor = -1;
+        for (int color = 0; color < k; color++) {
+            int cost = costs[0][color];
+            if (prevMin == -1 || cost < prevMin) {
+                prevSecondMin = prevMin;
+                prevMinColor = color;
+                prevMin = cost;
+            } else if (prevSecondMin == -1 || cost < prevSecondMin) {
+                prevSecondMin = cost;
             }
         }
-        return m1;
+
+        // And now, we need to work our way down, keeping track of the minimums.
+        for (int house = 1; house < n; house++) {
+            int min = -1; int secondMin = -1; int minColor = -1;
+            for (int color = 0; color < k; color++) {
+                // Determine the cost for this cell (without writing it in).
+                int cost = costs[house][color];
+                if (color == prevMinColor) {
+                    cost += prevSecondMin;
+                } else {
+                    cost += prevMin;
+                }
+                // Determine whether or not this current cost is also a minimum.
+                if (min == -1 || cost < min) {
+                    secondMin = min;
+                    minColor = color;
+                    min = cost;
+                } else if (secondMin == -1 || cost < secondMin) {
+                    secondMin = cost;
+                }
+            }
+            // Transfer current mins to be previous mins.
+            prevMin = min;
+            prevSecondMin = secondMin;
+            prevMinColor = minColor;
+        }
+
+        return prevMin;
     }
 };
