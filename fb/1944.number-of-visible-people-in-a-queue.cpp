9fb5d5d145d4bb0e27adaacf5d5679ef1a84c0d4
futureIsNow <ziqifan16@gmail.com>
2024-05-25 14:36:12 -0700

    Update 1944.number-of-visible-people-in-a-queue.cpp

--- fb/1944.number-of-visible-people-in-a-queue.cpp
+++ fb/1944.number-of-visible-people-in-a-queue.cpp
@@ -1,3 +1,7 @@
+// Time O(n)
+// Space O(n)
+// using stack to keep a sorted array that the current person is the shortest,
+// since the current one can only see the ones are taller than itself to the right
 class Solution {
 public:
     vector<int> canSeePersonsCount(vector<int>& heights) {
@@ -7,7 +11,7 @@ public:
           for(int i = heights.size()-1 ;i > =0 ;i--){
             int c = 1; // default as there would be one person in the right side higher than current one
             while(!st.empty() && heights[i] >= st.top()){
-                c++;
+                c++; // count how many are shorter than current one till the one taller than it to the right
                 st.pop();
             }
             if(st.empty()) c--; // minus 1 since the current person is the highest of all to the right
