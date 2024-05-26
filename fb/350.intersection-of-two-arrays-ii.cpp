114991d80669b48b2f8aef09efed0cb9c7612a3f
futureIsNow <ziqifan16@gmail.com>
2024-05-26 11:02:16 -0700

    Update and rename 350.Intersection-of-Two-Arrays-II.cpp to 350.intersection-of-two-arrays-ii.cpp

--- fb/350.intersection-of-two-arrays-ii.cpp
+++ fb/350.intersection-of-two-arrays-ii.cpp
@@ -1,20 +1,17 @@
 // Time O(m+n)
 // Space O(min(m,n))
-class Approach1 {
+class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()) {
-            return intersect(nums2, nums1);
-        }
         vector<int> ans;
-        unordered_map<int, int> m;
-        for (auto n : nums1) {
-            ++m[n];
+        unordered_map<int, int> um;
+        for(auto i : nums1) {
+            um[i]++;
         }
-        for (auto n : nums2) {
-            auto it = m.find(n);
-            if (it != end(m) && --it->second >= 0) {
-                ans.push_back(n);
+        for(auto i : nums2) {
+            if(um[i] > 0) {
+                ans.push_back(i);
+                um[i]--;
             }
         }
         return ans;
