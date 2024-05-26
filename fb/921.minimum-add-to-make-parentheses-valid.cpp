cf550aa0a6df3cf64060f87514206c1d158a010e
futureIsNow <ziqifan16@gmail.com>
2024-05-25 17:04:15 -0700

    Update 921.minimum-add-to-make-parentheses-valid.cpp

--- fb/921.minimum-add-to-make-parentheses-valid.cpp
+++ fb/921.minimum-add-to-make-parentheses-valid.cpp
@@ -4,12 +4,14 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
+        // track how many left or right parentheses standing alone
+        // and that will be the moves needed
         int l = 0, r = 0;
         for(auto i : s) {
             if(i == '(') {
                 l++;
             } else {
-                if(l > 0) {
+                if(l > 0) { // cancel out the right with the left
                     l--;
                 } else {
                     r++;
