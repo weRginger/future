89955356553e81921c7de1151800054a34a397cc
futureIsNow <ziqifan16@gmail.com>
2024-05-27 22:25:13 -0700

    Update 5.longest-palindromic-substring.cpp

--- fb/5.longest-palindromic-substring.cpp
+++ fb/5.longest-palindromic-substring.cpp
@@ -2,99 +2,36 @@
 // space: O(1)
 class Solution {
 public:
+    string expand(int i, int j, string s) {
+        int left = i;
+        int right = j;
+
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+
+        return s.substr(left + 1, right - left - 1); // left + 1 since the above while loop
+    }
+
     string longestPalindrome(string s) {
-        if(s.empty()) {
-            return s;
-        }
-        int ssize = s.size();
-        int begin = 0;
-        int end = 0;
-        for(int i = 0; i < ssize; i++) {
-            int j = 0;
-            // expand around a single letter - abcxcba
-            for(; i - j >= 0 && i + j < ssize && s[i-j] == s[i+j]; j++) {}
-            j--;
-            if(2 * j > end - begin) {
-                begin = i - j;
-                end = i + j;
+        string ans = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            string odd = expand(i, i, s);
+            if (odd.size() > ans.size()) {
+                ans = odd;
             }
-            // expand around two letters - abccba
-            for(j = 0; i - 1 - j >= 0 && i + j < ssize && s[i-1-j] == s[i+j]; j++) {}
-            j--;
-            if(2 * j + 1 > end - begin) {
-                begin = i - 1 - j;
-                end = i + j;
+
+            string even = expand(i, i + 1, s);
+            if (even.size() > ans.size()) {
+                ans = even;
             }
         }
-        return s.substr(begin, end - begin + 1);
+
+        return ans;
     }
 };
-
-// same as above, different coding style
-class Solution {
-public:
-    string longestPalindrome(string s) {
-        if(s.empty()) return s;
-        int start = 0, end = 0;
-        for(int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
-        }
-        return s.substr(start, end - start + 1);
-    }
-private:
-    int expandAroundCenter(string s, int left, int right) {
-        int L = left, R = right;
-        while(L >= 0 && R < s.length() && s[L] == s[R]) {
-            L--;
-            R++;
-        }
-        return R - L - 1;
-    }
-};
-
-// Manacher's O(n)
-class Solution {
-public:
-    string longestPalindrome(string const& s) {
-        int sLen = s.length();
-
-        // use the integer 1000 as the "boundary character"
-        // the input string can include any ASCII character
-        int len = sLen * 2 + 1;
-        vector<int> chars(len, 1000);
-        for (int i = 0; i < sLen; ++i) chars[2 * i + 1] = s[i];
-
-        int maxLen = 0, resCenter = 0;
-        int rightMost = -1, mirror = -1;
-        vector<int> radius(len, 0);
-
-        for (int i = 0; i < len; ++i) {
-            int r = i < rightMost ? min(radius[mirror * 2 - i], rightMost - i) : 0;
-            int left = i - 1 - r, right = i + 1 + r;
-
-            while (right >= 0 && right < len && chars[left] == chars[right]) --left, ++right;
-            radius[i] = r = --right - i;
-
-            if (right > rightMost) {
-                rightMost = right;
-                mirror = i;
-
-                // optimization detail:
-                // if right does not exceed rightMost
-                // we are sure that r cannot replace maxLen
-                if (r > maxLen) {
-                    maxLen = r;
-                    resCenter = i;
-                }
-            }
-        }
-
         return s.substr((resCenter - maxLen) / 2, maxLen);
     }
 };
