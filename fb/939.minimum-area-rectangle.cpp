5d6bac5bb96968fa9a7da297cc90006c66058260
futureIsNow <ziqifan16@gmail.com>
2024-06-08 16:00:56 -0700

    Update 939.minimum-area-rectangle.cpp

--- fb/939.minimum-area-rectangle.cpp
+++ fb/939.minimum-area-rectangle.cpp
@@ -17,12 +17,12 @@ public:
         for(int i = 0; i < points.size(); i++) { 
             for(int j = i + 1; j < points.size(); j++) { 
                 int x1 = points[i][0], y1 = points[i][1];  // Coordinates of Point A
-				int x2 = points[j][0], y2 = points[j][1];  // Coordinates of Point B
-                
-                if(x1 != x2 && y1 != y2) // Point A & B must form a diagonal of the rectangle.
-                    if(point_set.find({x1, y2}) != point_set.end()
-					   && point_set.find({x2, y1}) != point_set.end())
-                        minimum_area = min( minimum_area , abs(x1-x2) * abs(y1-y2) ); // Store the minimum area possible
+		int x2 = points[j][0], y2 = points[j][1];  // Coordinates of Point B
+                if(x1 != x2 && y1 != y2) { // Point A & B must form a diagonal of the rectangle.
+                    if(point_set.find({x1, y2}) != point_set.end() && point_set.find({x2, y1}) != point_set.end()) {
+                        minimum_area = min(minimum_area , abs(x1-x2) * abs(y1-y2)); // Store the minimum area possible
+		    }
+		}
             }
         }
         return minimum_area != INT_MAX ? minimum_area : 0; // Return 0 if no rectangle exists
