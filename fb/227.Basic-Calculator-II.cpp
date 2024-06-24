1c24b9772a82a961c228dccef60ea2c1a8d2fdf1
futureIsNow <ziqifan16@gmail.com>
2024-06-23 21:05:43 -0700

    Update 227.Basic-Calculator-II.cpp

--- fb/227.Basic-Calculator-II.cpp
+++ fb/227.Basic-Calculator-II.cpp
@@ -1,3 +1,47 @@
+// Time O(n)
+// Space O(1)
+// Author: Ziqi new
+class Solution {
+public:
+    int calculate(string s) {
+        s.push_back('+'); // add an op in the end to force calculation of the last number
+        long ans = 0;
+        long pre = 0;
+        long cur = 0;
+        char sign = '+';
+        for(auto c : s) {
+            if(c == ' ') continue;
+            if(c >= '0' && c <= '9') {
+                cur = 10 * cur + c - '0';
+            } else {
+                switch (sign) {
+                    case '+':
+                        ans += cur;
+                        pre = cur;
+                        break;
+                    case '-':
+                        ans -= cur;
+                        pre = -cur;
+                        break;
+                    case '*':
+                        ans -= pre;
+                        pre = pre * cur;
+                        ans += pre;
+                        break;
+                    case '/':
+                        ans -= pre;
+                        pre = pre / cur;
+                        ans += pre;
+                        break;
+                }
+                cur = 0;
+                sign = c; 
+            }
+        }
+        return ans;
+    }
+};
+
 // Author: Ziqi
 // Time O(n)
 // Space O(1)
