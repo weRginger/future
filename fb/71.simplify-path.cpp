5dbca9e030dc9a15f8619cc9d9678c82410621cb
futureIsNow <ziqifan16@gmail.com>
2024-06-24 19:32:46 -0700

    Update 71.simplify-path.cpp

--- fb/71.simplify-path.cpp
+++ fb/71.simplify-path.cpp
@@ -4,12 +4,20 @@ class Solution {
 public:
     string simplifyPath(string path) {
         string res, tmp;
-        vector<string> stk;
+        // should not use stack since res needs to start from leftmost
+        // see line 20
+        vector<string> stk; 
         istringstream ss(path);
         while(getline(ss,tmp,'/')) {
-            if (tmp == "" or tmp == ".") continue;
-            if (tmp == ".." && !stk.empty()) stk.pop_back();
-            else if (tmp != "..") stk.push_back(tmp);
+            if(tmp == "" or tmp == ".") continue;
+            if(tmp == "..") {
+                if(!stk.empty()) {
+                    stk.pop_back();
+                }
+            }
+            else {
+                stk.push_back(tmp);
+            }
         }
         for(auto str : stk) res += "/"+str;
         return res.empty() ? "/" : res;
