7c6cc2d4cb1a555e6020fb78bb8af67343daf1af
futureIsNow <ziqifan16@gmail.com>
2024-06-28 11:30:16 -0700

    Update 1047.remove-all-adjacent-duplicates-in-string.cpp

--- fb/1047.remove-all-adjacent-duplicates-in-string.cpp
+++ fb/1047.remove-all-adjacent-duplicates-in-string.cpp
@@ -1,5 +1,31 @@
 // Time O(n)
 // Space O(n)
+// author ziqi
+// note: using vector as stack to avoid the reverse in the end
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        vector<char> st;
+        st.push_back(s[0]);
+        for(int i = 1; i < s.size(); i++) {
+            if(st.empty()) {
+                st.push_back(s[i]);
+            } else if(s[i] == st.back()) {
+                st.pop_back();
+            } else {
+                st.push_back(s[i]);
+            }
+        }
+        string ans;
+        for(auto c : st) {
+            ans.push_back(c);
+        }
+        return ans;
+    }
+};
+
+// Time O(n)
+// Space O(n)
 class Solution {
 public:
     string removeDuplicates(string s) {
