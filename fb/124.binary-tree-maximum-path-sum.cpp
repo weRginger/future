0c1425dd98ab78be52d60123157887578b08ff6b
futureIsNow <ziqifan16@gmail.com>
2024-07-03 14:53:15 -0700

    Update 124.binary-tree-maximum-path-sum.cpp

--- fb/124.binary-tree-maximum-path-sum.cpp
+++ fb/124.binary-tree-maximum-path-sum.cpp
@@ -1,3 +1,35 @@
+// Time O(n)
+// Space O(h)
+// LC solution
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        maxSum = INT_MIN;
+        gainFromSubtree(root);
+        return maxSum;
+    }
+
+private:
+    int maxSum;
+    // post order traversal of subtree rooted at `root`
+    int gainFromSubtree(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        // add the path sum from left subtree. Note that if the path
+        // sum is negative, we can ignore it, or count it as 0.
+        // This is the reason we use `max` here.
+        int gainFromLeft = max(gainFromSubtree(root->left), 0);
+        // add the path sum from right subtree. 0 if negative
+        int gainFromRight = max(gainFromSubtree(root->right), 0);
+        // if left or right path sum are negative, they are counted
+        // as 0, so this statement takes care of all four scenarios
+        maxSum = max(maxSum, gainFromLeft + gainFromRight + root->val);
+        // return the max sum for a path starting at the root of subtree
+        return max(gainFromLeft + root->val, gainFromRight + root->val);
+    }
+};
+
 // Time O(N)
 // Space O(H)
 // ziqi's solution
@@ -31,40 +63,6 @@ public:
         return ans;
     }
 };
-
-// Time O(n)
-// Space O(h)
-// LC solution
-class Solution {
-public:
-    int maxPathSum(TreeNode* root) {
-        maxSum = INT_MIN;
-        gainFromSubtree(root);
-        return maxSum;
-    }
-
-private:
-    int maxSum;
-    // post order traversal of subtree rooted at `root`
-    int gainFromSubtree(TreeNode* root) {
-        if (root == nullptr) {
-            return 0;
-        }
-        // add the path sum from left subtree. Note that if the path
-        // sum is negative, we can ignore it, or count it as 0.
-        // This is the reason we use `max` here.
-        int gainFromLeft = max(gainFromSubtree(root->left), 0);
-        // add the path sum from right subtree. 0 if negative
-        int gainFromRight = max(gainFromSubtree(root->right), 0);
-        // if left or right path sum are negative, they are counted
-        // as 0, so this statement takes care of all four scenarios
-        maxSum = max(maxSum, gainFromLeft + gainFromRight + root->val);
-        // return the max sum for a path starting at the root of subtree
-        return max(gainFromLeft + root->val, gainFromRight + root->val);
-    }
-};
-
-
 
 /**
  * Definition for a binary tree node.
