4b2ee148c0fe09d23a79387d365d4137e7bd7c7b
futureIsNow <ziqifan16@gmail.com>
2024-07-04 11:19:36 -0700

    Update and rename 39.Combination-Sum.cpp to 39.combination-sum.cpp

---
+++ fb/39.combination-sum.cpp
@@ -0,0 +1,24 @@
+// Time, not sure, O(n^n)?
+// Space O(n)
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        if(candidates.empty()) return ans;
+        sort(candidates.begin(), candidates.end());
+        vector<int> curr;
+        helper(candidates, target, 0, curr, ans);
+        return ans;
+    }
+    void helper(vector<int>& candidates, int left, int idx, vector<int>& curr, vector<vector<int>>& ans) {
+        if(left == 0) {
+            ans.push_back(curr);
+            return;
+        }
+        for(int i = idx; i < candidates.size() && left > 0; i++) {
+            curr.push_back(candidates[i]);
+            helper(candidates, left - candidates[i], i, curr, ans);
+            curr.pop_back();
+        }
+    }
+};
