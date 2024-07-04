f641064186f262f78954b0bd9e19d962df4cc096
futureIsNow <ziqifan16@gmail.com>
2024-07-04 14:13:07 -0700

    Update and rename 123 to 981.time-based-key-value-store.cpp

---
+++ openai/981.time-based-key-value-store.cpp
@@ -0,0 +1,43 @@
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<int,string>>> time_map;
+public:
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        time_map[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if(!time_map.count(key)) {
+            return "";
+        }
+        if (timestamp < time_map[key][0].first) {
+            return "";
+        }
+        auto v = time_map[key];
+        int l = 0, r = v.size() - 1;
+        while(l + 1 < r) {
+            int m = l + (r - l) / 2;
+            if(timestamp <= v[m].first) {
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+        if(v[r].first <= timestamp) {
+            return v[r].second;
+        } else if(v[l].first <= timestamp) {
+            return v[l].second;
+        }
+        return "";
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
