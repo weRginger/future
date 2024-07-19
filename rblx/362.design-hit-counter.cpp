b448518939d72a376cf86b7f2fc8abb473a42974
futureIsNow <ziqifan16@gmail.com>
2024-07-18 23:35:09 -0700

    Update 362.design-hit-counter.cpp

--- rblx/362.design-hit-counter.cpp
+++ rblx/362.design-hit-counter.cpp
@@ -1,3 +1,45 @@
+// Time O(1) for both hit, gitHits (amortized) and ctor
+// Space O(n)
+// this is the optimmized solution for LC follow up q: What if the number of hits per second could be huge? Does your design scale?
+class HitCounter {
+private:
+    int past_sec = 300;
+    int total;
+    queue<pair<int, int>> q_; // pair.first is the ts, second is the count for this ts
+public:
+    HitCounter() {
+        total = 0;
+    }
+    
+    void hit(int timestamp) {
+        if(q_.empty() || q_.back().first != timestamp) {
+            q_.push({timestamp, 1});
+        } else {
+            q_.back().second++;
+        }
+        total++;
+    }
+    
+    int getHits(int timestamp) {
+        if(timestamp <= past_sec) {
+            return total;
+        }
+        while(!q_.empty()) {
+            auto front = q_.front();
+            if(front.first <= timestamp - past_sec) {
+                q_.pop();
+                total -= front.second;
+            } else {
+                return total;
+            }
+        }
+        return 0;
+    }
+};
+
+// Time O(1) for both hit, gitHits (amortized) and ctor
+// Space O(n)
+// no optimized for follow up q
 class HitCounter {
 private:
     int past_sec = 300;
